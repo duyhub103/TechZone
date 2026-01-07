e011e8fa742532a6bd3702685e2c44782ac18d64
duyhub103 <haogduy12345@gmail.com>
2026-01-08 01:28:46 +0700

    update Unique Index to review on db context

---
+++ Migrations/20260107182651_AddUniqueIndexToReview.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueIndexToReview : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_ProductId",
+                table: "Reviews");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_ProductId_UserId",
+                table: "Reviews",
+                columns: new[] { "ProductId", "UserId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_ProductId_UserId",
+                table: "Reviews");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_ProductId",
+                table: "Reviews",
+                column: "ProductId");
+        }
+    }
+}
